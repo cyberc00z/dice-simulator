db2b6a422456783552246ec39a8ea70863d35624
xadhrit <maharajrabel@gmail.com>
2021-11-26 13:13:40 +0530

    bug fix

--- dice.py
+++ dice.py
@@ -28,13 +28,14 @@ roll_dice(20)
 
 class MyWindow:
       def __init__(self, win)  :
-            self.lbl1 = Label(win, text='Gui of Die Sides')
+            self.lbl1 = Label(win, text=' # of Die Sides')
             self.lbl2 = Label(win, text='Roll Result ')
             self.lbl3 = Label(win, text='Dice Rolling Simulator', font=("Halvetica",20))
             self.t1 = Entry()
             self.t2 = Entry()
             self.btn1 = Button(win, text='Roll Dice')
             self.lbl1.place(x=100,y=100)
+            self.t1.place(x=200,y=100)
             self.b1 = Button(win, text='Roll Dice', font=("Halvetica",16), command=self.roll)
             self.b1.place(x=200, y=140)
             self.b1.config(height=1, width=8)
@@ -44,9 +45,15 @@ class MyWindow:
 
       def roll(self):
             self.t2.delete(0, 'end')
-            n = int(self.t1.get()) 
-            result = randint(1, n)
-            self.t2.insert(END, str(result))
+            #print(self.t1.get())
+            try:
+               n = int(self.t1.get())
+               print(n)
+               result = randint(1,n)
+               self.t2.insert(END,str(result))
+            except Exception as e:
+               print(e)
+            
 
 
 window = Tk()
